500a6f768d5504e1f41498d151c87e41f3ef6faa
roycrippen <roycrippen@gmail.com>
2025-02-01 21:56:46 -0500

    par execution and individual day running

---
+++ src/bin/aoc2024.rs
@@ -0,0 +1,77 @@
+use lib::day_01::solve_day_01;
+use lib::day_02::solve_day_02;
+use lib::day_03::solve_day_03;
+use lib::day_04::solve_day_04;
+use lib::day_05::solve_day_05;
+use lib::day_06::solve_day_06;
+use lib::day_07::solve_day_07;
+use lib::day_08::solve_day_08;
+use lib::day_09::solve_day_09;
+use lib::day_10::solve_day_10;
+use std::collections::HashMap;
+
+use clap::Parser;
+use lib::common::Solution;
+use rayon::prelude::*;
+use std::time::Instant;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Run all AOC 2024 days in parallel
+    #[arg(short, long, action)]
+    par: bool,
+
+    /// Run one AOC day
+    #[arg(short, long, default_value = None)]
+    day: Option<u8>,
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("{:?}", args);
+
+    let fs = vec![
+        solve_day_01,
+        solve_day_02,
+        solve_day_03,
+        solve_day_04,
+        solve_day_05,
+        solve_day_06,
+        solve_day_07,
+        solve_day_08,
+        solve_day_09,
+        solve_day_10,
+    ];
+
+    let days_m: HashMap<u8, fn() -> Solution> = fs
+        .iter()
+        .enumerate()
+        .map(|(i, f)| ((i + 1) as u8, *f))
+        .collect();
+
+    let start = Instant::now();
+    let mut solutions: Vec<Solution> = Vec::new();
+    if let Some(day) = args.day {
+        println!("Running day: {}", day);
+        if let Some(f) = days_m.get(&day) {
+            let solution = f();
+            println!(
+                "{:<30}, {:<30}, {:?}",
+                solution.part_a, solution.part_b, solution.duration
+            );
+        }
+    } else if args.par && args.day.is_none() {
+        fs.par_iter().map(|&f| f()).collect_into_vec(&mut solutions);
+    } else {
+        solutions = fs.iter().map(|&f| f()).collect();
+    }
+    println!();
+    for solution in solutions {
+        println!(
+            "{:<30}, {:<30}, {:?}",
+            solution.part_a, solution.part_b, solution.duration
+        );
+    }
+    println!("\ntotal elapsed time: {:?}", start.elapsed());
+}
